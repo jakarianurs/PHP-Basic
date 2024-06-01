8ce4c70d3deba4fc0243408d80d8f0d9b44717e4
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-01 19:49:58 +0600

    Create PHP Echo and Print.php

---
+++ PHP Echo and Print.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+?>
+
+</body>
+</html>
